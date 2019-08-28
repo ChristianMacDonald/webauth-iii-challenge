10052afcfa41abf47a8a161d68a67035bec63fea
ChristianMacDonald <chrismacd412@gmail.com>
2019-08-27 22:31:51 -0400

    Created boilerplate for server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const port = 8000;
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}.`);
+});
