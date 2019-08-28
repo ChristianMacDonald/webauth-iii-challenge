c83af2df4d4918faf0fd3ea841a873d4839873e5
ChristianMacDonald <chrismacd412@gmail.com>
2019-08-27 22:53:39 -0400

    Added functionality to register a new user

--- apiRouter.js
+++ apiRouter.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const userModel = require('./users/userModel');
 
 const router = express.Router();
+
+router.post('/register', validateUser, async (req, res) => {
+    try {
+        let credentials = req.body;
+        let hash = bcrypt.hashSync(credentials.password, 14);
+        credentials.password = hash;
+        let newUser = await userModel.insert(credentials);
+        res.status(201).json(newUser);
+    } catch (err) {
+        res.status(500).json({ error: 'There was an error saving the user to the database.' });
+    }
+});
 
 router.get('/users', async (req, res) => {
     try {
@@ -11,5 +24,21 @@ router.get('/users', async (req, res) => {
         res.status(500).json('The users information could not be retrieved.');
     }
 });
+
+function validateUser(req, res, next) {
+    if (req.body) {
+        if (req.body.username) {
+            if (req.body.password) {
+                next();
+            } else {
+                res.status(400).json('Missing required password field.');
+            }
+        } else {
+            res.status(400).json('Missing required username field.');
+        }
+    } else {
+        res.status(400).json('Missing user data.');
+    }
+}
 
 module.exports = router;
