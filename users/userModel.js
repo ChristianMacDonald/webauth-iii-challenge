cc98c07b473f689c571890a634d040197867993f
ChristianMacDonald <chrismacd412@gmail.com>
2019-08-27 22:29:42 -0400

    Created userModel.js

---
+++ users/userModel.js
@@ -0,0 +1,19 @@
+const db = require('../data/dbConfig');
+
+function get(id = 0) {
+    if (id) {
+        return db('users').where({ id });
+    } else {
+        return db('users');
+    }
+}
+
+async function insert(user) {
+    let [id] = await db('users').insert(user);
+    return get(id);
+}
+
+module.exports = {
+    get,
+    insert
+};
